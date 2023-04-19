5be7eb032d78700120cccce903a742367e1cd653
Jorge Sanchez Gonzalez <j.b.sanchez.gonzalez@accenture.com>
2023-04-19 08:29:05 +0200

    add cluster ecs to stack

--- lib/cdk_primer-stack.ts
+++ lib/cdk_primer-stack.ts
@@ -1,4 +1,10 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
+import * as s3 from "@aws-cdk/aws-s3";
+import { env } from 'process';
+
 // import * as sqs from '@aws-cdk/aws-sqs';
 
 export class CdkPrimerStack extends cdk.Stack {
@@ -6,10 +12,27 @@ export class CdkPrimerStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "MyVpc", {maxAzs: 2});
 
+    const cluster = new ecs.Cluster(this, "MyCluster", {vpc: vpc});
+
+      new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+
+          cluster: cluster,
+
+          taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
+
+          publicLoadBalancer: true
+
+  });
     // example resource
     // const queue = new sqs.Queue(this, 'CdkPrimerQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
   }
 }
+const app = new cdk.App();
+const jur1 = { account: '444455556666', region:'us-west-1'};
+const jur2 = { account: '123456789012', region:'us-west-2'};
+new CdkPrimerStack(app, 'CdkPrimerStack', {env: jur1});
+new CdkPrimerStack(app, 'CdkPrimerStack2', {env: jur2});
